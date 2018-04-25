b527ac3b977cc09608ae162ca2fec258cb841a6a
wencaizhang <1052642137@qq.com>
2018-04-25 23:30:29 +0800

    update src/pages/TrainOpinion.vue

--- src/pages/TrainOpinion.vue
+++ src/pages/TrainOpinion.vue
@@ -3,92 +3,28 @@
    	<div class="info-box">
 			<x-header>培训项目反映评估及改进书意见</x-header>
 			<table class="key-value-table">
-				<tr>
-					<td class="key">培训项目名称：</td>
-					<td class="value">{{ data.trainName }}</td>
-				</tr>
-				<tr>
-					<td class="key">承办部门：</td>
-					<td class="value">{{ data.undertakeDepartment }}</td>
-				</tr>
-				<tr>
-					<td class="key">项目负责人：</td>
-					<td class="value">{{ data.projectPerson }}</td>
-				</tr>
-				<tr>
-					<td class="key">办班时间：</td>
-					<td class="value">{{ data.startTime }}</td>
+				<tr v-for="item in database[0].data" :key="item.title">
+					<td class="key">{{ item.title }}：</td>
+					<td class="value">{{ item.model }}</td>
 				</tr>
 			</table>
-			<h4>项目管理：</h4>
-			<div class="questions-box">
-				<group title="项目负责人工作：">
-					<cell title="" :inline-desc="data" primary="content">
-						<range v-model="data.personWork" @on-change="onChange" :step=10></range>
-					</cell>
-				</group>
-				<group title="培训设施场地：">
-					<cell title="" :inline-desc="data" primary="content">
-						<range v-model="data.projectLocal" @on-change="onChange" :step=10></range>
-					</cell>
-				</group>
-				<group title="餐饮服务水平：">
-					<cell title="" :inline-desc="data" primary="content">
-						<range v-model="data.foodLevel" @on-change="onChange" :step=10></range>
-					</cell>
-				</group>
-				<group title="住宿服务水平：">
-					<cell title="" :inline-desc="data" primary="content">
-						<range v-model="data.innLevel" @on-change="onChange" :step=10></range>
-					</cell>
-				</group>
+
+			<div v-for="(item, index) in database" v-if="index > 0" :key="item.title">
+				<h4>{{ item.title }}</h4>
+				<div class="questions-box">
+					<group  v-for="i in item.data" :key="i.title" :title="i.title + '：' +  i.model">
+						<cell primary="content">
+							<range v-model="i.model" :step="i.step"></range>
+						</cell>
+					</group>
+				</div>
 			</div>
-			<h4>培训质量：</h4>
-			<div class="questions-box">
-				<group title="内容，方法符合需求：">
-					<cell title="" :inline-desc="data" primary="content">
-						<range v-model="data.contentLevel" @on-change="onChange" :step=10></range>
-					</cell>
-				</group>
-				<group title="内容难易适宜程度：">
-					<cell title="" :inline-desc="data" primary="content">
-						<range v-model="data.contentDifficultyLevel" @on-change="onChange" :step=10></range>
-					</cell>
-				</group>
-				<group title="培训内容掌握程度：">
-					<cell title="" :inline-desc="data" primary="content">
-						<range v-model="data.contentMasterLevel" @on-change="onChange" :step=10></range>
-					</cell>
-				</group>
-				<group title="教材资料质量：">
-					<cell title="" :inline-desc="data" primary="content">
-						<range v-model="data.bookLevel" @on-change="onChange" :step=10></range>
-					</cell>
-				</group>
-			</div>
-			<h4>教师评价：</h4>
-			<div class="questions-box">
-				<group title="专业技术水平：">
-					<cell title="" :inline-desc="data" primary="content">
-						<range v-model="data" @on-change="onChange" :step=10></range>
-					</cell>
-				</group>
-				<group title="授课技巧交流能力：">
-					<cell title="" :inline-desc="data" primary="content">
-						<range v-model="data" @on-change="onChange" :step=10></range>
-					</cell>
-				</group>
-				<group title="敬业及责任心：">
-					<cell title="" :inline-desc="data" primary="content">
-						<range v-model="data" @on-change="onChange" :step=10></range>
-					</cell>
-				</group>
-			</div>
+
 			<h4>改进意见：</h4>
 			<group>
-				<x-textarea :max="20" v-model="data.advise" :placeholder="placeholder" @on-focus="onEvent('focus')" @on-blur="onEvent('blur')"></x-textarea>
+				<x-textarea :max="20" v-model="data.advise" :placeholder="placeholder"></x-textarea>
 			</group>
-			<toast v-model="showPositionValue" type="text" :time="1500" is-show-mask text="请填写完成后保存" width="15em" position="top"></toast>
+			<toast v-model="showPositionValue" type="text" :time="1500" is-show-mask text="请填写完整后保存" width="15em" position="top"></toast>
 			<div class="btn-group">
 				<XButton class="btn" text="保存" type="primary" @click.native="save"></XButton>
 				<XButton class="btn" text="返回" type="primary"></XButton>
@@ -107,30 +43,57 @@ import {
   GroupTitle,
   Cell,
   XTextarea,
-  XHeader
+  XHeader,
+	Toast
 } from "vux";
 export default {
   data() {
     return {
       placeholder: "随便说点什么吧...",
+			showPositionValue: false,
       data: {
-				trainName: '',  //培训项目名称
-				undertakeDepartment: '',  //承办部门
-				projectPerson: '',  //项目负责人
-				startTime: '',  //开始时间
-				personWork: '',  //项目负责人工作分数
-				projectLocal: '',  //培训设施场地分数
-				foodLevel: '',  //餐饮分数
-				innLevel: '',  //住宿分数
-				contentLevel: '',  //内容，方法符合要求分数
-				contentDifficultyLevel: '',  //内容难易适合分数
-				contentMasterLevel: '',  //内容掌握程度分数
-				bookLevel: '',  //教材质量
-				advise: '',  //建议
-				// 这两个没用上，另外教师评价类下三个没有对应的字段
-				activity: '',  //活动id
-				activityName: '',  //活动名称
-			}
+				advise: '',  //改进意见
+				// activity: 0,  //活动id
+				// activityName: 0,  //活动名称
+			},
+			database: [
+				{ 
+					type: '', 
+					title: '', 
+					data: [  
+						{ model: '', name: 'trainName', title: '培训项目名称' },
+						{ model: '', name: 'undertakeDepartment', title: '承办部门' },
+						{ model: '', name: 'projectPerson', title: '项目负责人' },
+						{ model: '', name: 'startTime', title: '办班时间' },
+					]
+				}, { 
+					type: 'score',
+					title: '项目管理',
+					data: [
+						{ model: 0, step: 10, name: 'personWork', title: '项目负责人工作' },
+						{ model: 0, step: 10, name: 'projectLocal', title: '培训设施场地' },
+						{ model: 0, step: 10, name: 'foodLevel', title: '餐饮服务水平' },
+						{ model: 0, step: 10, name: 'innLevel', title: '住宿服务水平' },
+					]
+				}, { 
+					type: 'score', 
+					title: '培训质量', 
+					data: [
+						{ model: 0, step: 10, name: 'contentLevel', title: '内容，方法符合需求' },
+						{ model: 0, step: 10, name: 'contentDifficultyLevel', title: '内容难易适宜程度' },
+						{ model: 0, step: 10, name: 'contentMasterLevel', title: '培训内容掌握程度' },
+						{ model: 0, step: 10, name: 'bookLevel', title: '教材资料质量' },
+					]
+				}, { 
+					type: 'score', 
+					title: '教师评价', 
+					data: [
+						{ model: 0, step: 10, name: 'testa', title: '专业技术水平' },
+						{ model: 0, step: 10, name: 'testb', title: '授课技巧交流能力' },
+						{ model: 0, step: 10, name: 'testc', title: '敬业及责任心' },
+					]
+				},
+			]
     };
   },
   components: {
@@ -140,10 +103,12 @@ export default {
     Cell,
     XButton,
     XTextarea,
-    XHeader
+    XHeader,
+		Toast 
   },
   methods: {
     save () {
+			this._getData()
       const { data } = this;
       const values = Object.values(data);
       const emptyValues = values.filter(v => !v);
@@ -153,6 +118,14 @@ export default {
       }
       this._sendData(data);
     },
+		_getData () {
+			const { data, database } = this;
+			database.forEach(item => {
+				item.data.forEach(item2 => {
+					data[item2.name] = item2.model;
+				})
+			});
+		},
     _sendData (data) {
       axios({
         url: '/question/research/save',
@@ -166,7 +139,7 @@ export default {
     },
     _errHandler (err) {
 
-    }
+    },
   },
 };
 </script>
@@ -176,5 +149,8 @@ export default {
 }
 h4 {
   margin: 30px 0 10px;
+}
+.weui-cell {
+	height: 30px;
 }
 </style>
